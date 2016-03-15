b1d54fc3255f9d6f780a64f5b9d834c0e260d1d2
liangpuhe <liangpuhe@icloud.com>
2016-03-15 23:06:43 +0800

    update 3-15

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,87 @@
+
 $(function() {
 
-  var $window           = $(window),
-      win_height_padded = $window.height() * 1.1,
-      isTouch           = Modernizr.touch;
+    var $window = $(window),
+        $slide = $('.homeSlide'),
+        $body = $('body'),
+        win_height_padded = $window.height() * 1.1,
+        isTouch = Modernizr.touch;
 
-  if (isTouch) { $('.revealOnScroll').addClass('animated'); }
+    if (isTouch) {
+        $('.revealOnScroll').addClass('animated');
+    }
 
-  $window.on('scroll', revealOnScroll);
+    $window.on('scroll', revealOnScroll);
 
-  // Init Skrollr
-  var s = skrollr.init();
-  
-  function revealOnScroll() {
-    var scrolled = $window.scrollTop(),
-        win_height_padded = $window.height() * 1.1;
 
-    // Showed...
-    $(".revealOnScroll:not(.animated)").each(function () {
-      var $this     = $(this),
-          offsetTop = $this.offset().top;
 
-      if (scrolled + win_height_padded > offsetTop) {
-        if ($this.data('timeout')) {
-          window.setTimeout(function(){
-            $this.addClass('animated ' + $this.data('animation'));
-          }, parseInt($this.data('timeout'),10));
-        } else {
-          $this.addClass('animated ' + $this.data('animation'));
+    //FadeIn all sections   
+    $body.imagesLoaded(function() {
+
+        adjustWindow();
+//         destroyPreloader();
+    });
+
+    function destroyPreloader() {
+        var objLoading = document.getElementById("preloader").style.display = "none";
+        var mainLoading = document.getElementById("main-page").style.display = "";
+    }
+
+
+
+    function adjustWindow() {
+
+        // Init Skrollr
+        var s = skrollr.init();
+
+        // Get window size
+        winH = $window.height();
+
+        // Keep minimum height 550
+        if (winH <= 550) {
+            winH = 550;
         }
-      }
-    });
-    // Hidden...
-   $(".revealOnScroll.animated").each(function (index) {
-      var $this     = $(this),
-          offsetTop = $this.offset().top;
-      if (scrolled + win_height_padded < offsetTop) {
-        $(this).removeClass('fadeInDown animated bounce')
-      }
-    });
-  }
 
-  revealOnScroll();
+        // Resize our slides
+        $slide.height(winH+50);
+
+        // Refresh Skrollr after resizing our sections
+        s.refresh($('.homeSlide'));
+
+    }
+
+
+
+
+    function revealOnScroll() {
+        var scrolled = $window.scrollTop(),
+            win_height_padded = $window.height() * 1.1;
+
+        // Showed...
+        $(".revealOnScroll:not(.animated)").each(function() {
+            var $this = $(this),
+                offsetTop = $this.offset().top;
+
+            if (scrolled + win_height_padded > offsetTop) {
+                if ($this.data('timeout')) {
+                    window.setTimeout(function() {
+                        $this.addClass('animated ' + $this.data('animation'));
+                    }, parseInt($this.data('timeout'), 10));
+                } else {
+                    $this.addClass('animated ' + $this.data('animation'));
+                }
+            }
+        });
+        // Hidden...
+        $(".revealOnScroll.animated").each(function(index) {
+            var $this = $(this),
+                offsetTop = $this.offset().top;
+            if (scrolled + win_height_padded < offsetTop) {
+                $(this).removeClass('fadeInDown animated bounce')
+            }
+        });
+    }
+
+    revealOnScroll();
+
 });
